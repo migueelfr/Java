f130d30a16e1d82a5486499862a14e83a38eeb05
Miguel F. Vins <142853940+migueelfr@users.noreply.github.com>
2024-03-20 22:25:01 -0300

    Delete Aluno directory

--- Aluno/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args) {
-        Aluno aluno1 = new Aluno("Miguel",19,"213213");
-        System.out.println("Informações do aluno");
-        System.out.println("Nome: " + aluno1.getNome());
-        System.out.println("Idade: " + aluno1.getIdade());
-        System.out.println("Matrícula: " + aluno1.getMatricula());
-    }
-}
